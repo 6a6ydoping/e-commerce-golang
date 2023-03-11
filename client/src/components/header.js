053c62b2fa7b1896279416ac6e273c90f0344392
Ali <a_emamian@kbtu.kz>
2023-03-11 14:42:36 +0600

    header update 2

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SearchBar from "./searchBar";
 import "./styles/header.css";
 import "./styles/header.css";
 function Header(props) {
@@ -21,25 +22,7 @@ function Header(props) {
                 </svg>
                 <h3 className="store_name">Game Store</h3>
             </div>
-            <div className="searchBar__container">
-                <input
-                    className="searchBar"
-                    placeholder="Search games..."
-                ></input>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 48 48"
-                    className="NavBar_svg__wdLyI"
-                    id="search_logo"
-                    aria-label="Search"
-                    style={{
-                        fill: "#ccc",
-                    }}
-                    {...props}
-                >
-                    <path d="M39.8 41.95 26.65 28.8q-1.5 1.3-3.5 2.025-2 .725-4.25.725-5.4 0-9.15-3.75T6 18.75q0-5.3 3.75-9.05 3.75-3.75 9.1-3.75 5.3 0 9.025 3.75 3.725 3.75 3.725 9.05 0 2.15-.7 4.15-.7 2-2.1 3.75L42 39.75Zm-20.95-13.4q4.05 0 6.9-2.875Q28.6 22.8 28.6 18.75t-2.85-6.925Q22.9 8.95 18.85 8.95q-4.1 0-6.975 2.875T9 18.75q0 4.05 2.875 6.925t6.975 2.875Z" />
-                </svg>
-            </div>
+            <SearchBar />
             <div className="end__container">
                 <div className="github__container">
                     <svg
@@ -61,7 +44,7 @@ function Header(props) {
                     </svg>
                     <h3>
                         <a href="https://github.com/6a6ydoping">
-                            <h3>6a6ydoping</h3>
+                            <h3 className="github">6a6ydoping</h3>
                         </a>
                     </h3>
                 </div>
--- client/src/components/searchBar.js
+++ client/src/components/searchBar.js
@@ -41,70 +41,32 @@ function SearchBar(props) {
 
     const handleSubmit = async () => {};
 
-    if (items.length === 0) {
-        return (
-            <>
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
-                />
-                <div className="searchBar__container">
-                    <form className="searchBar__form" onSubmit={handleSubmit}>
-                        <input
-                            type="text"
-                            placeholder="Enter name of the item..."
-                            value={searchString}
-                            onChange={(e) => setSearchString(e.target.value)}
-                        />
-                        <button type="button" onClick={handleClick}>
-                            <span className="material-symbols-outlined">
-                                search
-                            </span>
-                        </button>
-                        <select
-                            name="filterBy"
-                            id="filterBy"
-                            onChange={(e) => handleFilterByChange(e)}
-                        >
-                            <option value="rating">Rating</option>
-                            <option value="price">Price</option>
-                        </select>
-                    </form>
-                    <p>No items found</p>
-                </div>
-            </>
-        );
-    }
-
     return (
         <>
-            <link
-                rel="stylesheet"
-                href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
-            />
             <div className="searchBar__container">
                 <form className="searchBar__form" onSubmit={handleSubmit}>
                     <input
-                        type="text"
-                        placeholder="Enter name of the item..."
+                        className="searchBar"
+                        placeholder="Search games..."
                         value={searchString}
                         onChange={(e) => setSearchString(e.target.value)}
-                    />
+                    ></input>
                     <button type="button" onClick={handleClick}>
-                        <span className="material-symbols-outlined">
-                            search
-                        </span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 48 48"
+                            className="NavBar_svg__wdLyI"
+                            id="search_logo"
+                            aria-label="Search"
+                            style={{
+                                fill: "#ccc",
+                            }}
+                            {...props}
+                        >
+                            <path d="M39.8 41.95 26.65 28.8q-1.5 1.3-3.5 2.025-2 .725-4.25.725-5.4 0-9.15-3.75T6 18.75q0-5.3 3.75-9.05 3.75-3.75 9.1-3.75 5.3 0 9.025 3.75 3.725 3.75 3.725 9.05 0 2.15-.7 4.15-.7 2-2.1 3.75L42 39.75Zm-20.95-13.4q4.05 0 6.9-2.875Q28.6 22.8 28.6 18.75t-2.85-6.925Q22.9 8.95 18.85 8.95q-4.1 0-6.975 2.875T9 18.75q0 4.05 2.875 6.925t6.975 2.875Z" />
+                        </svg>
                     </button>
-                    <select
-                        name="filterBy"
-                        id="filterBy"
-                        onChange={handleFilterByChange}
-                    >
-                        <option value="rating">Rating</option>
-                        <option value="price">Price</option>
-                    </select>
                 </form>
-                <ItemList items={items} />
             </div>
         </>
     );
